3cd72cfa0b859b82562412f8dd6193c8368459ff
iddimsangi <adammsangi@gmail.com>
2021-04-18 14:14:21 +0300

    add middleware and testing

--- src/index.js
+++ src/index.js
@@ -6,13 +6,23 @@ import registerServiceWorker from './registerServiceWorker';
 // import  reducer  from './Store/reducer';
 import counterReducers from './Store/reducers/counter';
 import resultReducers from './Store/reducers/result';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 const rootReducers = combineReducers({
     ctr_red: counterReducers,
     res_red: resultReducers
-})
-const store = createStore(rootReducers);
+});
+const logger = store =>{
+    return next =>{
+        return action =>{
+            console.log('[Middleware] Dispatching ',action);
+            const result = next(action);
+            console.log('[Middleware] next state',store.getState());
+            return result;
+        }
+    }
+}
+const store = createStore(rootReducers, applyMiddleware(logger));
 
 ReactDOM.render(<Provider store = {store}><App/></Provider>, document.getElementById('root'));
 // ReactDOM.render(<App/>, document.getElementById('root'));
